c7d6183d7d8a223f4608fad763c78db94d5f82a8
Abdallah Farag <99741501+Al0olo@users.noreply.github.com>
2022-07-07 08:36:01 +0200

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -22,5 +22,40 @@ int main()
 	for (int i = 0; i < 4; i++)
 		cout << h[i] << " ";
 
+	// One of the functions in vector is push_back which allow you to append one element in the vector.
+	h.push_back(50);
+
+	cout << endl;
+	for (int i = 0; i < 5; i++)
+		cout << h[i] << " ";
+
+	// Another way to initialize the vector is
+	vector <int> a = { 1,2,3,4,5 };
+
+	cout << endl;
+	for (int i = 0; i < 5; i++)
+		cout << a[i] << " ";
+
+	// Another way is to copy elements of another vector
+	vector <int> a2(a);
+
+	cout << endl;
+	for (int i = 0; i < 5; i++)
+		cout << a2[i] << " ";
+
+	// Another way is to pass two parameters in the constructor one for size another for the default value.
+	vector <int>c(5, 10);
+
+	cout << endl;
+	for (int i = 0; i < 5; i++)
+		cout << c[i] << " ";
+
+	// One another function in vector is swap, which allows you to swap two vectors
+	a.swap(c);
+
+	cout << endl;
+	for (int i = 0; i < 5; i++)
+		cout << c[i] << " ";
+
 	return 0;
 }
